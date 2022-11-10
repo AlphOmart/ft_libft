4675ff66ebca4fcf29ed38bb6a468813facfc3a1
AlphOmArt <menelik.wubneh@gmail.com>
2022-11-10 20:23:35 +0100

    ok

---
+++ ft_strdup.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mwubneh <mwubneh@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/10 20:10:31 by mwubneh           #+#    #+#             */
+/*   Updated: 2022/11/10 20:10:31 by mwubneh          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+char    *strdup(const char  *s)
+{
+    char    *str;
+    int     size;
+    int     i;
+
+    size = 0;
+    i = 0;
+    while (s[size])
+        size++;
+    str = malloc(size * sizeof(char));
+    if(str == NULL)
+        return (str);
+    while (str[i])
+        str[i] = s[i++];
+    str[i] = '\0';
+    return (str);
+}
