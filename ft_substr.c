907038aec836d24fce227daf4875ce5b662e7adf
alphOmArt <menelik.wubneh@gmail.com>
2022-11-12 12:52:24 +0100

    bref

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,25 @@
 /*   By: mwubneh <mwubneh@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 22:45:51 by mwubneh           #+#    #+#             */
-/*   Updated: 2022/11/11 22:45:51 by mwubneh          ###   ########.fr       */
+/*   Updated: 2022/11/12 12:52:14 by mwubneh          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
+
+char	*ft_substr(char const s1, unsigned int start, size_t len)
+{
+	char	*s2;
+	int		i;
+
+	i = 0;
+	s2 = malloc (sizeof (char) * len);
+	if (s2 == NULL)
+		return (NULL);
+	while (len)
+	{
+		s2[i++] = s[start + i];
+		len--;
+	}
+	return (&s2);
+}
