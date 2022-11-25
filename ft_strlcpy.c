89244cccdc0b581591df493307dadb5e5e181052
alphOmArt <menelik.wubneh@gmail.com>
2022-11-25 17:15:25 +0100

    ok on change de jeu

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: mwubneh <mwubneh@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 12:44:21 by mwubneh           #+#    #+#             */
-/*   Updated: 2022/11/12 16:42:04 by mwubneh          ###   ########lyon.fr   */
+/*   Updated: 2022/11/25 17:15:09 by mwubneh          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,11 @@ size_t	ft_strlcpy(char	*dst, const char	*src, size_t n)
 	size_t	i;
 
 	i = 0;
-	if (n == 0)
-		return (0);
-	while (src[i] && i < n - 1)
+	while (src[i] && i < (n - 1))
 	{
 		dst[i] = src[i];
 		i++;
 	}
 	dst[i] = '\0';
-	i--;
-	return (i);
+	return (ft_strlen(src));
 }
