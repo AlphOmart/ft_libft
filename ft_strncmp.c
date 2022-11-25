91ab726479733de639d1a3af4666ac4789ba5613
alphOmArt <menelik.wubneh@gmail.com>
2022-11-25 19:38:56 +0100

    ok

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mwubneh <mwubneh@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 15:07:28 by mwubneh           #+#    #+#             */
-/*   Updated: 2022/11/25 19:11:14 by mwubneh          ###   ########lyon.fr   */
+/*   Updated: 2022/11/25 19:38:51 by mwubneh          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while (i < n - 1 && s1 && s2 && s1[i] == s2[i])
 			i++;
 	if (s1[i] != s2[i])
-		return (s1[i] - s2[i]);
+		return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
 	else
 		return (0);
 }
