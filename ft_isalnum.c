fc411a6d6b10f29eaea439a34d18ab441cff142b
alphOmArt <menelik.wubneh@gmail.com>
2022-11-25 15:04:01 +0100

    ok on avance bien

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,31 +6,15 @@
 /*   By: mwubneh <mwubneh@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 15:41:01 by mwubneh           #+#    #+#             */
-/*   Updated: 2022/11/12 16:27:56 by mwubneh          ###   ########lyon.fr   */
+/*   Updated: 2022/11/25 15:03:51 by mwubneh          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isdigit( int c)
-{
-	if ('0' <= c && c <= '9')
-		return (1);
-	else
-		return (0);
-}
-
-int	ft_isalpha( int b)
-{
-	if (('a' <= b && b <= 'z') || ('A' <= b && b <= 'Z'))
-		return (1);
-	else
-		return (0);
-}
-
 int	ft_isalnum(int a)
 {
-	if (ft_isalpha(a) == 1 && ft_isdigit(a) == 1)
+	if (ft_isalpha(a) == 1 || ft_isdigit(a) == 1)
 		return (1);
 	else
 		return (0);
