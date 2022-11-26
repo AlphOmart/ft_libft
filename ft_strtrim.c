94d81fd24f2a862dd944c279201def35e8869e92
alphOmArt <menelik.wubneh@gmail.com>
2022-11-26 03:02:43 +0100

    vsiujhdv

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: mwubneh <mwubneh@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 02:11:41 by mwubneh           #+#    #+#             */
-/*   Updated: 2022/11/26 02:31:07 by mwubneh          ###   ########lyon.fr   */
+/*   Updated: 2022/11/26 03:00:49 by mwubneh          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,21 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	i;
-	size_t	len;
-	char	*s1cpy;
+	const char	*s1cpy;
 
 	if (s1 == NULL)
 		return (NULL);
 	if (set == NULL)
 		return (ft_strdup(s1));
-	len = ft_strlen(s1) + 1;
-	s1cpy = ft_calloc(s1, len);
-
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	if (*s1 == '\0')
+		return (calloc(1, sizeof(char )));
+	s1cpy = s1;
+	while (*s1)
+		s1++;
+	s1--;
+	while (ft_strchr(set, *s1))
+		s1--;
+	return (ft_substr(s1cpy, 0, s1 - s1cpy + 1));
 }
