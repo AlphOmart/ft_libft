4bea8b6c3c2fdda605a08aeec08e3a4a5e08a8bf
AlphOmArt <menelik.wubneh@gmail.com>
2022-12-02 10:38:46 +0100

    kjb

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: mwubneh <mwubneh@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 18:30:00 by mwubneh           #+#    #+#             */
-/*   Updated: 2022/11/25 22:07:58 by mwubneh          ###   ########lyon.fr   */
+/*   Updated: 2022/12/02 10:38:42 by mwubneh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 void	*ft_memmove(void	*dst, const void	*src, size_t	n)
 {
-	char	*start;
+	void	*start;
 
-	start = (char *)dst;
+	start = dst;
 	if (src < dst)
 		ft_memcpy(dst, src, n);
 	else if (dst < src)
