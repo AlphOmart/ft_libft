4b80f5ef345fef2fca893dafb1acb63bf512c68c
AlphOmArt <menelik.wubneh@gmail.com>
2023-01-13 10:34:25 +0100

    xvz

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,11 +6,13 @@
 /*   By: mwubneh <mwubneh@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/17 18:13:31 by mwubneh           #+#    #+#             */
-/*   Updated: 2022/12/17 18:13:35 by mwubneh          ###   ########.fr       */
+/*   Updated: 2023/01/12 22:04:54 by mwubneh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+
 
 void	*ft_memmove(void	*dst, const void	*src, size_t	n)
 {
